a48d0cbe2b950552cb2e499e98d0e6fa093c3767
phanvankha <120581197+phanvankha@users.noreply.github.com>
2023-04-13 15:33:18 +0700

    Delete ducngu directory

--- ducngu/script.js
+++
@@ -1,7 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET', '1', true);
-xhr.setRequestHeader('Authorization', 'Bearer YOUR_API_KEY');
-xhr.onreadystatechange = function () {
-  // Xử lý kết quả ở đây
-};
-xhr.send();
